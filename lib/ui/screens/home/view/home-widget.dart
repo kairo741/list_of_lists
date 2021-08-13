4bb2152817e56aba1226ece617ac9ec0e1226363
Kairo <kairo.controlenamao@gmail.com>
2021-08-13 01:25:23 -0300

    Tela home que contém as listas do Usuário

--- lib/ui/screens/home/view/home-widget.dart
+++ lib/ui/screens/home/view/home-widget.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:list_of_lists/core/controller/lista/lista-controller.dart';
 import 'package:list_of_lists/core/database/services/lista-service.dart';
 import 'package:list_of_lists/core/entity/lista.dart';
+import 'package:list_of_lists/core/utils/constants.dart';
 import 'package:list_of_lists/ui/shared-components/shared-app-bar.dart';
+import 'package:list_of_lists/ui/shared-components/shared-list-tiles.dart';
 
 import 'home-page.dart';
 
 class HomeWidget extends State<HomePage> {
-  var _service;
   Future<List<Lista>>? _list;
+
+  ListaController controller = ListaController();
 
   @override
   void initState() {
     super.initState();
-    _service = GetIt.I.get<ListaService>();
+    var _service = GetIt.I.get<ListaService>();
     _list = _service.find();
   }
 
@@ -24,43 +28,50 @@ class HomeWidget extends State<HomePage> {
         title: "Home",
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
       ),
-      body: FutureBuilder(
-          future: _list,
-          builder: (context, future) {
-            if (!future.hasData) {
-              return Center(
-                  child: CircularProgressIndicator(
-                backgroundColor: Colors.cyan,
-              ));
-            } else {
-              List<Lista> list = future.data as List<Lista>;
-              return ListView.builder(
-                  itemCount: list.length,
-                  itemBuilder: (context, i) {
-                    var listObject = list[i];
-                    return ListTile(
-                        leading: Icon(Icons.person),
-                        title: Text(
-                          listObject.name,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(listObject.name),
-                        trailing: Container(
-                          width: 100,
-                          child: Row(
-                            children: [
-                              IconButton(
-                                  icon: Icon(Icons.edit),
-                                  onPressed: () {
-                                    // _back.goToForm(context, listObject);
-                                  }),
-                              // iconRemoveButton(context, listObject)
-                            ],
-                          ),
-                        ));
-                  });
-            }
-          }),
+      body: ListView(
+        children: [
+          FutureBuilder(
+              future: _list,
+              builder: (context, future) {
+                if (!future.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Center(
+                        child: CircularProgressIndicator(
+                      backgroundColor: Colors.cyan,
+                    )),
+                  );
+                } else {
+                  List<Lista> list = future.data as List<Lista>;
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: list.length,
+                      itemBuilder: (context, i) {
+                        var lista = list[i];
+                        if (lista.status == Constants.ACTIVE) {
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 8.0, bottom: 2),
+                            child: SharedListTiles(
+                              title: lista.name,
+                              icon: lista.icon,
+                              onTap: () {},
+                              editButton: () {
+                                controller.listaController = lista;
+                                controller.editLista(context);
+                              },
+                            ),
+                          );
+                        } else {
+                          return Container(
+                            height: 0,
+                            width: 0,
+                          );
+                        }
+                      });
+                }
+              }),
+        ],
+      ),
     );
   }
 }
