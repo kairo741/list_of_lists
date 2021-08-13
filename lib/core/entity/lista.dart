7aaa2977365ae2abcd7b40516ef6b69f0c1ffeaa
Kairo <kairo.controlenamao@gmail.com>
2021-08-13 01:21:57 -0300

    Inserção do campo de Icon no objeto da lista

--- lib/core/entity/lista.dart
+++ lib/core/entity/lista.dart
@@ -1,19 +1,22 @@
 class Lista {
   static const String ID = "id";
   static const String NAME = "name";
+  static const String ICON = "icon";
   static const String ID_USER = "id_user";
   static const String CREATE_DATE = "create_date";
   static const String STATUS = "status";
 
   int? id;
   String name;
+  int icon;
   int idUser;
   String status;
   DateTime? createDate;
 
   Lista(
-      { this.id,
+      {this.id,
       required this.name,
+      required this.icon,
       required this.idUser,
       required this.status,
       this.createDate});
