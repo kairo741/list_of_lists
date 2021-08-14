3852acbc99fc13f7e43eecf750eee3b63e7af6eb
Kairo <kairo.controlenamao@gmail.com>
2021-08-14 02:11:02 -0300

    Implementação para finalização do CRUD de inserção de Lista

--- lib/ui/screens/home/view/home-widget.dart
+++ lib/ui/screens/home/view/home-widget.dart
@@ -1,56 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:list_of_lists/core/controller/lista/lista-controller.dart';
+import 'package:list_of_lists/core/controller/user/user-controller.dart';
 import 'package:list_of_lists/core/entity/lista.dart';
 import 'package:list_of_lists/core/utils/constants.dart';
 import 'package:list_of_lists/ui/shared-components/shared-alert-box.dart';
 import 'package:list_of_lists/ui/shared-components/shared-app-bar.dart';
 import 'package:list_of_lists/ui/shared-components/shared-list-tiles.dart';
+import 'package:list_of_lists/ui/shared-components/shared-text-pop-up.dart';
+import 'package:list_of_lists/ui/styles/app-colors.dart';
 import 'package:list_of_lists/ui/styles/app_text_styles.dart';
 
 import 'home-page.dart';
 
 class HomeWidget extends State<HomePage> {
-  ListaController controller = ListaController();
+  ListaController listaController = ListaController();
+  UserController userController = UserController();
+
+  var user;
 
   @override
   void initState() {
     super.initState();
+    _getUser();
+  }
+
+  _getUser() async {
+    user = await userController.getUser(); // todo fazer tela de carregamento para carregar usuário
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SharedAppBar(
-        title: "Home",
+        title: "Seja bem vindo, " + user.name + "!",
         actions: [
           IconButton(
               onPressed: () {
                 setState(() {
-                  controller.refreshList();
+                  listaController.refreshList();
                 });
               },
               icon: Icon(Icons.refresh)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          IconButton(
+              onPressed: () {
+                _newLista(context);
+              },
+              icon: Icon(Icons.add)),
         ],
       ),
       body: ListView(
+        // shrinkWrap: true,
         children: [
           Observer(builder: (context) {
             return FutureBuilder(
-                future: controller.list,
+                future: listaController.list,
                 builder: (context, future) {
                   if (!future.hasData) {
                     return Padding(
                       padding: const EdgeInsets.all(20.0),
                       child: Center(
                           child: CircularProgressIndicator(
-                        backgroundColor: Colors.cyan,
+                        backgroundColor: AppColors.kSecondaryColor,
                       )),
                     );
                   } else {
                     List<Lista> list = future.data as List<Lista>;
                     return ListView.builder(
+                        physics: ScrollPhysics(),
                         shrinkWrap: true,
                         itemCount: list.length,
                         itemBuilder: (context, i) {
@@ -61,38 +78,14 @@ class HomeWidget extends State<HomePage> {
                                   const EdgeInsets.only(top: 8.0, bottom: 2),
                               child: SharedListTiles(
                                 title: lista.name,
-                                icon: lista.icon,
+                                icon: lista.icon!,
                                 onTap: () {},
                                 editButton: () {
-                                  controller.listaController = lista;
-                                  controller.editLista(context);
+                                  listaController.listaController = lista;
+                                  listaController.editLista(context);
                                 },
                                 deleteButton: () {
-                                  showDialog(
-                                      context: context,
-                                      builder: (context) => SharedAlertBox(
-                                            content: RichText(
-                                              text: TextSpan(
-                                                  text:
-                                                      "Essa ação excluirá a lista ",
-                                                  style: AppTextStyles.body16,
-                                                  children: [
-                                                    TextSpan(
-                                                        text: lista.name,
-                                                        style: AppTextStyles
-                                                            .textBlackBold)
-                                                  ]),
-                                            ),
-                                            title: "Tem certeza?",
-                                            actionButtonText: "Excluir",
-                                            actionButtonOnPressed: () {
-                                              controller.removeLista(lista.id!);
-                                              Navigator.of(context).pop();
-                                            },
-                                          ));
-
-                                  // controller.listaController = lista;
-                                  // controller.editLista(context);
+                                  _deleteLista(context, lista);
                                 },
                               ),
                             );
@@ -109,5 +102,48 @@ class HomeWidget extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  _newLista(BuildContext context) {
+    var newLista;
+    return showDialog(
+        context: context,
+        builder: (context) => SharedTextPopUp(
+              onChanged: (value) {
+                newLista = Lista(name: value, idUser: user.id!);
+              },
+              actionButtonText: "Criar",
+              hintText: "Nome da lista",
+              title: "Nova Lista",
+              icon: Icons.add_box_outlined,
+              actionButtonOnPressed: () {
+                if (newLista != null) {
+                  listaController.preSaveLista(context, newLista);
+                  Navigator.of(context).pop();
+                }
+              },
+            ));
+  }
+
+  _deleteLista(BuildContext context, Lista lista) {
+    return showDialog(
+        context: context,
+        builder: (context) => SharedAlertBox(
+              content: RichText(
+                text: TextSpan(
+                    text: "Essa ação excluirá a lista ",
+                    style: AppTextStyles.body16,
+                    children: [
+                      TextSpan(
+                          text: lista.name, style: AppTextStyles.textBlackBold)
+                    ]),
+              ),
+              title: "Tem certeza?",
+              actionButtonText: "Excluir",
+              actionButtonOnPressed: () {
+                listaController.removeLista(lista.id!);
+                Navigator.of(context).pop();
+              },
+            ));
   }
 }
